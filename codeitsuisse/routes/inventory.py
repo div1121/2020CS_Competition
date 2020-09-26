1d51c25982c76c35d3f1e6bc2bf1d38ff6750e23
div1121 <jasonchu19991121@gmail.com>
2020-09-26 13:39:22 +0800

    hi

--- codeitsuisse/routes/inventory.py
+++ codeitsuisse/routes/inventory.py
@@ -21,17 +21,18 @@ def evaluateinventory():
     count = 0
     for s in itemlist:
         r = findstr(s,realstr)
+        print(r)
         if count < 10:
             result.append(r)
             count+=1
-        #else:
-            #heapq.heapify(result)
-            #heapq.heappush(r)
-            #heapq.heappop(result)
+        else:
+            result.append(r)
+    result.sort()
     print(result)
     ab = []
-    for c in result:
-        ab.append(c[1])
+    for i,c in enumerate(result):
+        if i<10:
+            ab.append(c[1])
     fp = [{"searchItemName":realstr,"items":ab}]
     logging.info("My result :{}".format(fp))
     return json.dumps(fp)
@@ -39,29 +40,48 @@ def evaluateinventory():
 def findstr(s,realstr):
     n = len(s)
     m = len(realstr)
-    dp = [[0] * (n+1)] * (m+1)
-    for i in range(1,m+1):
-        for j in range(1,n+1):
-            if realstr[i-1]==s[j-1]:
+    dp = [[0] * (n+1) for _ in range (m+1)]
+    print(s,realstr,len(dp))
+    for i in range(m+1):
+        for j in range(n+1):
+            #if (i-1>=0 and j-1>=0):
+                #print(realstr[i-1].lower(),s[j-1].lower(),(realstr[i-1].lower())==(s[j-1].lower()))
+            if (i==0 or j==0):
+                #print(i,j,max(i,j))
+                dp[i][j] = max(i,j)
+            elif (realstr[i-1].lower())==(s[j-1].lower()):
+                #print(i,j,i-1,j-1,dp[i-1][j-1])
                 dp[i][j] = dp[i-1][j-1]
-            else: 
-                dp[i][j] = min(dp[i-1][j-1],min(dp[i-1][j],dp[i][j-1])) + 1
+            else:
+                dp[i][j] = min(dp[i-1][j-1],dp[i-1][j],dp[i][j-1]) + 1
+            #print(i,j,dp[i][j],end=" ")
+        #print()
     realans = ""
+    print("Real ans:" , dp[m][n])
     i = m
     j = n
     while (i>0 and j>0):
-        if realstr[i-1]==s[j-1]:
+        if (realstr[i-1].lower())==(s[j-1].lower()):
             realans = realstr[i-1] + realans
             i-=1
             j-=1
         else:
-            t = min(dp[i-1][j-1],min(dp[i-1][j],dp[i][j-1]))
+            t = min(dp[i-1][j-1],dp[i-1][j],dp[i][j-1])
             if t==dp[i-1][j-1]:
-                realans = s[j] + realans
+                realans = s[j-1] + realans
+                i-=1
+                j-=1
             elif t==dp[i-1][j]:
-                realans = "-" + realans[i] + realans
+                realans = "-" + realstr[i-1] + realans
+                i-=1
             else:
-                realans = "+" + s[j] + realans
+                realans = "+" + s[j-1] + realans
+                j-=1
+    print(i,j)
+    while i>0:
+        realans = "-" + realstr[i-1] + realans
+        i-=1
+
     print(dp[m][n],realans)
     return (dp[m][n],realans)
 
