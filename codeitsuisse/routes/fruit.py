c3fe01bb054cba6fcfa5ceadc1985862d5bbe4df
div1121 <jasonchu19991121@gmail.com>
2020-09-26 16:00:53 +0800

    a

--- codeitsuisse/routes/fruit.py
+++ codeitsuisse/routes/fruit.py
@@ -13,17 +13,17 @@ def evaluatefruit():
     data = request.get_data()
     #data = json.loads(data)
     #logging.info("data sent for evaluation {}".format(data))
-    data = ast.literal_eval(data)
-    a = data["maApple"]
-    b = data["maWatermelon"]
-    c = data["maBanana"]
-    ta = 30
-    tb = 30
-    tc = 30
-    print(a)
-    print(b)
-    print(c)
-    result = ta*a + tb*b + tc*c
+    #data = ast.literal_eval(data)
+    #a = data["maApple"]
+    #b = data["maWatermelon"]
+    #c = data["maBanana"]
+    #ta = 30
+    #tb = 30
+    #tc = 30
+    #print(a)
+    #print(b)
+    #print(c)
+    #result = ta*a + tb*b + tc*c
     logging.info("My result :{}".format(result))
     return jsonify("0")
 
